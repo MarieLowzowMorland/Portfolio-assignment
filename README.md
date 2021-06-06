a05167b4362b017a6fbf072bf4da9cde4477794e
Marie Lowzow Morland <marie@morland.no>
2021-06-06 12:23:48 +0200

    layout compelete

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Portfolio-assignment
 An overview of my three biggest projects during my first year of coding. The page is set up as a report for the changes and improvements I have made to the projects in the last two weeks.
+
+https://mm-first-year-projects.netlify.app/
--- js/pages/index.js
+++ js/pages/index.js
@@ -2,15 +2,44 @@ import addHeaderForPage, { pageNames } from "../templates/header.js";
 
 addHeaderForPage(pageNames.HOME);
 
-const landscapeSliceQuery = window.matchMedia("screen and (min-aspect-ratio: 4/3) and (max-aspect-ratio: 3/1) and (min-height: 30rem)");
+const landscapeSliceQuery = window.matchMedia(
+  "screen and (min-aspect-ratio: 4/3) and (max-aspect-ratio: 3/1), screen and (max-width: 992px)"
+);
+const portraitSliceQuery = window.matchMedia(
+  "screen and (min-aspect-ratio: 1/2) and (max-aspect-ratio: 4/3), screen and (max-width: 992px)"
+);
 
 const setSvgToLandscapeSliceIfApplicable = () => {
-  if(landscapeSliceQuery && landscapeSliceQuery.matches){
-    document.querySelector("#landscape > svg").setAttribute("preserveAspectRatio", "xMidYMid slice");
+  if (landscapeSliceQuery && landscapeSliceQuery.matches) {
+    document
+      .querySelector("#landscape > svg")
+      .setAttribute("preserveAspectRatio", "xMidYMid slice");
   } else {
-    document.querySelector("#landscape > svg").setAttribute("preserveAspectRatio", "xMidYMid meet");
-  };
+    document
+      .querySelector("#landscape > svg")
+      .setAttribute("preserveAspectRatio", "xMidYMid meet");
+  }
+};
+
+const setSvgToPortraitSliceIfApplicable = () => {
+  if (portraitSliceQuery && portraitSliceQuery.matches) {
+    document
+      .querySelector("#portrait > svg")
+      .setAttribute("preserveAspectRatio", "xMidYMid slice");
+  } else {
+    document
+      .querySelector("#portrait > svg")
+      .setAttribute("preserveAspectRatio", "xMidYMin meet");
+  }
 };
 
 setSvgToLandscapeSliceIfApplicable();
-landscapeSliceQuery.addEventListener("change", setSvgToLandscapeSliceIfApplicable);
+setSvgToPortraitSliceIfApplicable();
+landscapeSliceQuery.addEventListener(
+  "change",
+  setSvgToLandscapeSliceIfApplicable
+);
+portraitSliceQuery.addEventListener(
+  "change",
+  setSvgToPortraitSliceIfApplicable
+);
